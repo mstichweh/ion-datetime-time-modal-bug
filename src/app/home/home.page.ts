9795b8cc844b1c472b53d175910eae28bfda45b2
Mark Stichweh <mark.stichweh@rle.de>
2022-02-04 18:29:38 +0100

    Removed unnecessary code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DatetimeCustomEvent, IonModal } from '@ionic/angular';
 
 @Component({
@@ -12,7 +12,7 @@ export class HomePage {
 
   public timeValue = '10:30';
 
-  constructor(private readonly elementRef: ElementRef) {}
+  constructor() {}
 
   public async openModal(): Promise<void> {
     await this.modal.present();
@@ -20,44 +20,5 @@ export class HomePage {
 
   public onChange(e: Event): void {
     this.timeValue = (e as DatetimeCustomEvent).detail.value;
-    console.log(`Entered time: ${this.timeValue}`);
-  }
-
-  public setFocus(): void {
-    this.getInputElement()?.focus();
-  }
-
-  public setBlur(): void {
-    this.getInputElement()?.blur();
-  }
-
-  public onClosePopup(): void {
-    this.setFocused(false);
-  }
-
-  private async setFocused(focused: boolean): Promise<void> {
-    // Do not change focus, if just the popup opens and gains the focus
-    // if (!focused && this.popupOpened) {
-    //   return;
-    // }
-    // if (this.focusedValue === focused) {
-    //   return;
-    // }
-    // this.focusedValue = focused;
-    // // Mark as touch, when looses the focus the first time
-    // if (!this.touchedValue && !this.focusedValue) {
-    //   this.touchedValue = true;
-    //   this.onTouched();
-    //   this.propagateStateStyleClasses();
-    // }
-    // if (this.focusedValue && !this.popupOpened) {
-    //   this.popupOpened = true;
-    //   await this.popup?.present();
-    // }
-    // this.changedState();
-  }
-
-  private getInputElement(): HTMLInputElement {
-    return this.elementRef.nativeElement.querySelector('input.value-holder');
   }
 }
